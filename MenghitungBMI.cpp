c10c9f223d4514ec86a3ba6b21986aa7ff03b735
rrassya19-bit <rrassys19@gmail.com>
2026-03-10 12:25:19 +0700

    memperbaiki baris di bagian Fungsi untuk menentukan status BMI

--- MenghitungBMI.cpp
+++ MenghitungBMI.cpp
@@ -24,11 +24,14 @@ string statusBMI(float bmi)
 {
     if (bmi < 18.5) {
         return "Berat Badan Kurang";
-    } else if (bmi < 25) {
+    } 
+        else if (bmi < 25) {
         return "Berat Badan Normal";
-    } else if (bmi < 30) {
+    }
+        else if (bmi < 30) {
         return "Berat Badan Kelebihan";
-    } else {
+    } 
+        else {
         return "Obesitas";
     }
 }
