d9533ea222237eb503ba683bd8f4f1d74f42ed5a
rrassya19-bit <rrassys19@gmail.com>
2026-03-10 12:03:21 +0700

    menambahkan Prosedur untuk input data

--- MenghitungBMI.cpp
+++ MenghitungBMI.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+//Prosedur untuk input data
+void inputData(float & berat, float & tinggi)
+{
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
