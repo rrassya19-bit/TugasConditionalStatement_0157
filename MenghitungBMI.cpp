0fb8e35263162cfde0a441bf860d8b42168e6990
rrassya19-bit <rrassys19@gmail.com>
2026-03-10 12:11:26 +0700

    menambahkan fungsi uutama

--- MenghitungBMI.cpp
+++ MenghitungBMI.cpp
@@ -32,3 +32,23 @@ string statusBMI(float bmi)
         return "Obesitas";
     }
 }
+
+
+int main() {
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    //Memanggil prosedur input
+    inputData(berat, tinggi);
+
+    //Menghitung BMI
+    bmi = hitungBMI(berat, tinggi);
+
+    cout << endl;
+    cout << "--- Hasil ---" << endl;
+    cout << "BMI Anda : " << bmi << endl;
+    cout << "Status   : " << statusBMI(bmi) << endl;
+
+    return 0;
+}
